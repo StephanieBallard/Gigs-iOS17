bbc61546bcb8fa9c1313f944b65075ddb1231bae
Stephanie Ballard <user.name>
2020-05-05 14:38:33 -0700

    login/signup button tapped func completed

--- Gigs_iOS17/ViewControllers/LoginViewController.swift
+++ Gigs_iOS17/ViewControllers/LoginViewController.swift
@@ -14,9 +14,9 @@ enum LoginType {
 }
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var usernameTextField: UITextField!
-    @IBOutlet weak var UserTypeSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var userTypeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var loginSignupButton: UIButton!
     @IBOutlet weak var passwordTextField: UITextField!
     
@@ -25,7 +25,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         loginSignupButton.backgroundColor = UIColor(hue: 190/360, saturation: 70/100, brightness: 80/100, alpha: 1.0)
         loginSignupButton.tintColor = .white
         loginSignupButton.layer.cornerRadius = 8.0
@@ -46,17 +46,43 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginSignUpButtonTapped(_ sender: UIButton) {
-        
+        if let username = usernameTextField.text,
+            !username.isEmpty,
+            let password = passwordTextField.text,
+            !password.isEmpty {
+            let user = User(username: username, password: password)
+            if loginType == .signUp {
+                gigController?.signUp(with: user, completion: { result in
+                    do {
+                        let success = try result.get()
+                        if success {
+                            DispatchQueue.main.async {
+                                let alertController = UIAlertController(title: "Sign Up Successful", message: "Please log in", preferredStyle: .alert)
+                                let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                                alertController.addAction(alertAction)
+                                self.present(alertController, animated: true) {
+                                    self.loginType = .signIn
+                                    self.userTypeSegmentedControl.selectedSegmentIndex = 1
+                                    self.loginSignupButton.setTitle("Sign In", for: .normal)
+                                }
+                            }
+                        }
+                    } catch {
+                        print("Error signing up: \(error)")
+                    }
+                })
+            }
+        }
     }
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
