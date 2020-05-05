0c68d96372681dc7b6bc09bfb3a60ecf58f428cd
Stephanie Ballard <user.name>
2020-05-05 13:39:20 -0700

    completed sign up func

--- Gigs_iOS17/Controller/GigController.swift
+++ Gigs_iOS17/Controller/GigController.swift
@@ -6,4 +6,60 @@
 //  Copyright © 2020 Stephanie Ballard. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class GigController {
+    
+    enum HTTPMethod: String {
+        case get = "GET"
+        case post = "POST"
+    }
+    
+    enum NetworkError: Error {
+        case noData
+        case failedSignUp
+    }
+    
+    var bearer: Bearer?
+    
+    private let baseURL = URL(string: "https://lambdagigapi.herokuapp.com/api")!
+    private lazy var signUpURL = baseURL.appendingPathComponent("/users/signup")
+    private lazy var signInURL = baseURL.appendingPathComponent("/users/login")
+    
+    private lazy var jsonEncoder = JSONEncoder()
+    
+    func signUp(with user: User, completion: @escaping (Result<Bool, NetworkError>) -> Void) {
+        print("signUpURL = \(signUpURL.absoluteString)")
+        
+        var request = URLRequest(url: signUpURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+            
+            let task = URLSession.shared.dataTask(with: request) { _, response, error in
+                if let error = error {
+                    print("Sign up failed with error: \(error)")
+                    completion(.failure(.failedSignUp))
+                    return
+                }
+                
+                guard let response = response as? HTTPURLResponse,
+                    response.statusCode == 200 else {
+                        print("Sign up was unsuccessful")
+                        completion(.failure(.failedSignUp))
+                        return
+                }
+                
+                completion(.success(true))
+            }
+            task.resume()
+        } catch {
+            print("Error encoding user: \(error)")
+            completion(.failure(.failedSignUp))
+            return
+        }
+    }
+}
