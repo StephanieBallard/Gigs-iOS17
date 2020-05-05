e095cfab57d0f1112e6fdd953d875328358da5ab
Stephanie Ballard <user.name>
2020-05-05 14:24:21 -0700

    segmented control tapped func written

--- Gigs_iOS17/ViewControllers/LoginViewController.swift
+++ Gigs_iOS17/ViewControllers/LoginViewController.swift
@@ -8,24 +8,45 @@
 
 import UIKit
 
+enum LoginType {
+    case signUp
+    case signIn
+}
+
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var UserTypeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var loginSignupButton: UIButton!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    var gigController: GigController?
+    var loginType = LoginType.signUp
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        loginSignupButton.backgroundColor = UIColor(hue: 190/360, saturation: 70/100, brightness: 80/100, alpha: 1.0)
+        loginSignupButton.tintColor = .white
+        loginSignupButton.layer.cornerRadius = 8.0
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        usernameTextField.becomeFirstResponder()
     }
     
     @IBAction func segmentedControlTapped(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            loginType = .signUp
+            loginSignupButton.setTitle("Sign Up", for: .normal)
+        } else {
+            loginType = .signIn
+            loginSignupButton.setTitle("Sign In", for: .normal)
+        }
     }
     
     @IBAction func loginSignUpButtonTapped(_ sender: UIButton) {
+        
     }
     
     /*
