f12f235adbd09ed7c844c2a79b445668239db6e8
Stephanie Ballard <user.name>
2020-05-06 14:45:09 -0700

    outlets and action created and new model for gig created

---
+++ Gigs_iOS17/Models/Gig.swift
@@ -0,0 +1,15 @@
+//
+//  Gig.swift
+//  Gigs_iOS17
+//
+//  Created by Stephanie Ballard on 5/6/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import Foundation
+
+struct Gig: Codable {
+    let title: String
+    let dueDate: Date
+    let description: String
+}
---
+++ Gigs_iOS17/ViewControllers/GigDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GigDetailViewController.swift
+//  Gigs_iOS17
+//
+//  Created by Stephanie Ballard on 5/6/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import UIKit
+
+class GigDetailViewController: UIViewController {
+    @IBOutlet weak var gigTitleTextField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
